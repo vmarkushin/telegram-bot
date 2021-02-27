bd79ed139eb76620aa0fc6e3a969b6911078643e
Vladislav Markushin <negigic@gmail.com>
2021-02-28 00:53:48 +0300

    ChosenInlineResult

---
+++ raw/src/types/chosen_inline_result.rs
@@ -0,0 +1,17 @@
+use crate::{Location, User};
+
+#[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize)]
+pub struct ChosenInlineResult {
+    /// The unique identifier for the result that was chosen
+    pub result_id: String,
+    /// The user that chose the result
+    pub from: User,
+    /// Sender location, only for bots that require user location
+    pub location: Option<Location>,
+    /// Identifier of the sent inline message. Available only if there is an inline keyboard
+    /// attached to the message. Will be also received in callback queries and can be used
+    /// to edit the message
+    pub inline_message_id: Option<String>,
+    /// The query that was used to obtain the result
+    pub query: String,
+}
